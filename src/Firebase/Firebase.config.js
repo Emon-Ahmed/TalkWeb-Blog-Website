8f474dc88d5332c932feba34d7258201d29d8a0b
Emon Ahmed <technicalboyemon@gmail.com>
2022-01-01 17:09:08 +0600

    Server Setup

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7832MRINkIlLzaJSIddqxGa8m69NvGLE",
+  authDomain: "talkweb-blog.firebaseapp.com",
+  projectId: "talkweb-blog",
+  storageBucket: "talkweb-blog.appspot.com",
+  messagingSenderId: "490032380343",
+  appId: "1:490032380343:web:ce8141204ffad5e81d5032",
+};
+
+export default firebaseConfig;
